57cc8ddb38a20ca70dba7785d16e16b90adb8271
hammad3082 <mohammed.hammad@vxceed.com>
2026-06-10 23:52:42 +0530

    feat(auth): implement reactive login form and auth service integration

--- src/app/core/auth/services/auth.ts
+++ src/app/core/auth/services/auth.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginRequest, LoginResponse } from '../../../features/auth/models/login.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  private http = inject(HttpClient);
+
+  private apiUrl = "https://localhost:7254/api/Auth";
+
+  login(credentials: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, credentials);
+  }
+}
--- src/app/features/auth/pages/login/login.page.ts
+++ src/app/features/auth/pages/login/login.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
-
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Auth } from '../../../../core/auth/services/auth';
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
@@ -8,7 +8,47 @@ import { ReactiveFormsModule, FormControl } from '@angular/forms';
   styleUrl: './login.page.css',
 })
 export class LoginPage {
+  private authService = inject(Auth);
+
   pageTitle = 'Secure Login Portal';
 
-  emailControl = new FormControl('');
+  loginForm = new FormGroup({
+    username: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(6)]
+    })
+  });
+
+  get username() { return this.loginForm.get('username') as FormControl; }
+  
+  get password() { return this.loginForm.get('password') as FormControl; }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const loginPayload = this.loginForm.getRawValue();
+    console.log('Valid Form Payload:', loginPayload);
+
+    this.authService.login(loginPayload).subscribe(
+      {
+        next: (response) => {
+          console.log('Login successful! JWT Token received:', response.accessToken);
+
+          // TODO: Save token to localStorage and navigate to dashboard
+        },
+        error: (apiError) => {
+          console.log('Authentication failed:', apiError);
+        
+          // TODO: Display an error message banner on the UI
+        }
+      }
+    )
+  }
 }
