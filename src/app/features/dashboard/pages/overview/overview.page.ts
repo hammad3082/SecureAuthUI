31d7ac55eb531f4e17e982bfa669fc43eb62eea5
hammad3082 <mohammed.hammad@vxceed.com>
2026-07-11 23:58:53 +0530

    feat: implement dashboard

--- src/app/features/dashboard/pages/overview/overview.page.ts
+++ src/app/features/dashboard/pages/overview/overview.page.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
-import { Sample } from '../../services/sample';
+import { SessionDiagnostics } from '../../models/overview.model';
+import { Overview } from '../../services/overview';
 
 @Component({
   selector: 'app-overview',
@@ -8,26 +9,32 @@ import { Sample } from '../../services/sample';
   styleUrl: './overview.page.css',
 })
 export class OverviewPage {
-  private SampleService = inject(Sample);
+  private OverviewService = inject(Overview);
 
-  apiMessage = signal<string>('Loading secure message...');
-  errorMessage = signal<string | null>(null);
+  diagnostics = signal<SessionDiagnostics | null>(null);
+  isLoading = signal<boolean>(true);
+  errorMessage = signal<string>('');
 
-  RemoveTokens(): void{
-    this.SampleService.RemoveTokens();
+  ngOnInit() : void {
+    this.fetchDashboardData();
   }
 
-  ngOnInit() : void{
-    this.SampleService.getProtectedData().subscribe({
-      next: (message) => {
-        this.apiMessage.set(message);
-        console.log(message);
+  fetchDashboardData() : void {
+    this.OverviewService.getProtectedDiagnostics().subscribe({
+      next: (data) => {
+        this.diagnostics.set(data);
+        this.isLoading.set(false);
       },
       error: (err) => {
-        console.error('Interceptor failed:', err);
-        this.errorMessage.set('Failed to load secure data. Check network headers!');
-        this.apiMessage.set('');
+        console.error('Failed to load identity diagnostics:', err);
+
+        this.errorMessage.set('Failed to verify security context.');
+        this.isLoading.set(false);
       }
     });
   }
+  
+  // RemoveTokens(): void{
+  //   this.SampleService.RemoveTokens();
+  // }
 }
--- src/app/features/dashboard/services/overview.ts
+++ src/app/features/dashboard/services/overview.ts
@@ -1,22 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Storage } from '../../../core/auth/services/storage.service';
 import { environment } from '../../../../environments/environment';
+import { Storage } from '../../../core/auth/services/storage.service';
+import { SessionDiagnostics } from '../models/overview.model';
 @Injectable({
   providedIn: 'root',
 })
-export class Sample {
+export class Overview {
   private http = inject(HttpClient);
   private storage = inject(Storage)
 
   private readonly AUTH_KEY = 'auth_token';
   private readonly REFRESH_KEY = 'refresh_token';
 
-  private apiUrl = `${environment.apiUrl}/Sample`;
+  private apiUrl = `${environment.apiUrl}`;
+
+  getProtectedDiagnostics(): Observable<SessionDiagnostics> {
+    return this.http.get<SessionDiagnostics>(`${this.apiUrl}/Security/diagnostics`)
+  }
 
   getProtectedData(): Observable<string> {
-    return this.http.get(`${this.apiUrl}/protected`, {responseType: 'text'});
+    return this.http.get(`${this.apiUrl}/Sample/protected`, {responseType: 'text'});
   }
 
   RemoveTokens(): void{
